0a8979b1cc01f03d0031783c4f36485899b4608b
Segazzzs <mrakul986@gmail.com>
2026-02-27 16:21:15 +0200

    Finish all site

--- js/main.js
+++ js/main.js
@@ -2,27 +2,23 @@ const openBtn = document.getElementById("open");
 const closeBtn = document.getElementById("close");
 const modal = document.getElementById("modal");
 
-openBtn.addEventListener("click", function () {
-  openBtn.classList.add("close");
-  closeBtn.classList.remove("close");
-  modal.classList.add("is-open");
-});
-
-closeBtn.addEventListener("click", function () {
-  closeBtn.classList.add("close");
-  openBtn.classList.remove("close");
-  modal.classList.remove("is-open");
-});
-
 const yachtList = document.getElementById("card-list");
 const openListBtn = document.getElementById("open-card");
 
 openListBtn.addEventListener("click", function () {
+  let maxHeight = "478px";
+
+  if (yachtList.scrollWidth > 703) {
+    maxHeight = "508px";
+  }
+
   if (!yachtList.classList.contains("card-list-open")) {
     yachtList.style.maxHeight = yachtList.scrollHeight + "px";
     yachtList.classList.add("card-list-open");
   } else {
-    yachtList.style.maxHeight = "478px";
+    yachtList.style.maxHeight = maxHeight;
     yachtList.classList.remove("card-list-open");
   }
 });
+
+console.log(yachtList.scrollWidth);
